f4f8bc80e859d1cc70c5d14549ef80c7c5929f2d
mart337i <mart337i@gamil.com>
2023-12-14 11:13:23 +0100

    [ADD] Add more commands

--- main.py
+++ main.py
@@ -72,66 +72,6 @@ _logger = logging.getLogger(__name__)
 
 
 @bot.command()
-async def play(ctx: commands.Context, *, query: str) -> None:
-    """Play a song with the given query."""
-    if not ctx.guild:
-        return
-
-    player: wavelink.Player
-    player = cast(wavelink.Player, ctx.voice_client)  # type: ignore
-
-    if not player:
-        try:
-            player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # type: ignore
-        except AttributeError:
-            await ctx.send("Please join a voice channel first before using this command.")
-            return
-        except discord.ClientException:
-            await ctx.send("I was unable to join this voice channel. Please try again.")
-            return
-
-    # Turn on AutoPlay to enabled mode.
-    # enabled = AutoPlay will play songs for us and fetch recommendations...
-    # partial = AutoPlay will play songs for us, but WILL NOT fetch recommendations...
-    # disabled = AutoPlay will do nothing...
-    player.autoplay = wavelink.AutoPlayMode.enabled
-
-    # Lock the player to this channel...
-    if not hasattr(player, "home"):
-        player.home = ctx.channel
-    elif player.home != ctx.channel:
-        await ctx.send(f"You can only play songs in {player.home.mention}, as the player has already started there.")
-        return
-
-    # This will handle fetching Tracks and Playlists...
-    # Seed the doc strings for more information on this method...
-    # If spotify is enabled via LavaSrc, this will automatically fetch Spotify tracks if you pass a URL...
-    # Defaults to YouTube for non URL based queries...
-    tracks: wavelink.Search = await wavelink.Playable.search(query)
-    if not tracks:
-        await ctx.send(f"{ctx.author.mention} - Could not find any tracks with that query. Please try again.")
-        return
-
-    if isinstance(tracks, wavelink.Playlist):
-        # tracks is a playlist...
-        added: int = await player.queue.put_wait(tracks)
-        await ctx.send(f"Added the playlist **`{tracks.name}`** ({added} songs) to the queue.")
-    else:
-        track: wavelink.Playable = tracks[0]
-        await player.queue.put_wait(track)
-        await ctx.send(f"Added **`{track}`** to the queue.")
-
-    if not player.playing:
-        # Play now since we aren't playing anything...
-        await player.play(player.queue.get(), volume=30)
-
-    # Optionally delete the invokers message...
-    try:
-        await ctx.message.delete()
-    except discord.HTTPException:
-        pass
-
-@bot.command()
 async def sync(ctx: commands.Context):
     try:
         synced = await bot.tree.sync()
@@ -178,15 +118,81 @@ async def defaultfilters(ctx: commands.Context) -> None:
 
     await ctx.message.add_reaction("\u2705")
 
+@bot.command(name="toggle", aliases=["pause", "resume"])
+async def pause_resume(ctx: commands.Context) -> None:
+    """Pause or Resume the Player depending on its current state."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
 
-@bot.tree.command(name="test")
+    await player.pause(not player.paused)
+    await ctx.message.add_reaction("\u2705")
+
+
+@bot.command()
+async def volume(ctx: commands.Context, value: int) -> None:
+    """Change the volume of the player."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+
+    await player.set_volume(value)
+    await ctx.message.add_reaction("\u2705")
+
+
+@bot.command(aliases=["dc"])
+async def disconnect(ctx: commands.Context) -> None:
+    """Disconnect the Player."""
+    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+    if not player:
+        return
+
+    await player.disconnect()
+    await ctx.message.add_reaction("\u2705")
+
+
+#------------------------------------------------------------------------# Slash commands
+
+@bot.tree.command(name="test2")
 async def test(interaction : discord.Interaction):
     await interaction.response.send_message("This works")
+
+@bot.tree.command(name="sync")
+async def s_sync(interaction : discord.Interaction):
+    try:
+        synced = await bot.tree.sync()
+        _logger.info(f"synced {len(synced)} commands")
+        await interaction.response.send_message(f"synced {len(synced)} commands")
+    except Exception as e:
+        await interaction.response.send_message(f"Error {e}")
+        _logger.error(e)
+        return
+
+@bot.tree.command(name="disconnect")
+async def s_disconnect(interaction : discord.Interaction):
+    player = cast(wavelink.Player, interaction.guild.voice_client)
+
+    if not player:
+        return
+
+    await player.disconnect()
+
+@bot.tree.command(name="skip")
+async def s_skip(interaction : discord.Interaction):
+    player = cast(wavelink.Player, interaction.guild.voice_client)
+
+    if not player:
+        return
+    
+    song_name = player.current.title
+    await player.skip()
+
+    await interaction.response.send_message(f"skipped {song_name}")
 
 
 @bot.tree.command(name="play")
 @app_commands.describe(query = "query")
-async def slash_play(interaction : discord.Interaction, query:str):
+async def s_play(interaction : discord.Interaction, query:str):
 
     player: wavelink.Player
     player = cast(wavelink.Player, interaction.guild.voice_client) 
@@ -237,45 +243,8 @@ async def slash_play(interaction : discord.Interaction, query:str):
         await player.play(player.queue.get(), volume=30)
 
 
-    await interaction.response.send_message(f"This works {query}")
-
-
-@bot.command(name="toggle", aliases=["pause", "resume"])
-async def pause_resume(ctx: commands.Context) -> None:
-    """Pause or Resume the Player depending on its current state."""
-    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
-    if not player:
-        return
-
-    await player.pause(not player.paused)
-    await ctx.message.add_reaction("\u2705")
-
-
-@bot.command()
-async def volume(ctx: commands.Context, value: int) -> None:
-    """Change the volume of the player."""
-    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
-    if not player:
-        return
-
-    await player.set_volume(value)
-    await ctx.message.add_reaction("\u2705")
-
-
-@bot.command(aliases=["dc"])
-async def disconnect(ctx: commands.Context) -> None:
-    """Disconnect the Player."""
-    player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
-    if not player:
-        return
-
-    await player.disconnect()
-    await ctx.message.add_reaction("\u2705")
-
-
 async def main() -> None:
     async with bot:
         await bot.start(TOKEN)
-
 
 asyncio.run(main())
